a72e86f9b82d94cca62dfce0858e6649387a818c
Muhammed Bakgor <bakgormuhammed@gmail.com>
2022-08-05 21:04:56 +0300

    // UPDATE FOR ITEM ADDING. // QUICK TIP // THIS IS HOW YOU REGISTER ITEM TO MINECRAFT. // ItemGroup.MISC THAT DEFINES WHICH INVENTORY TAB TO REGISTER. // AFTER REGISTER, YOU NEED TO ADD SOME CODE TO en_us.json FILE. THERE IS AN EXAMPLE OVER THERE. // THEN YOU NEED TO CREATE A JSON FILE FOR ITEM LIKE example_item.json // FOR LAST, ADD ITEM TEXTURE FILE TO /textures/item AT .PNG FORMAT

--- src/main/java/net/mbkgr/abmod/ABMod.java
+++ src/main/java/net/mbkgr/abmod/ABMod.java
@@ -1,6 +1,7 @@
 package net.mbkgr.abmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.mbkgr.abmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,7 +13,6 @@ public class ABMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/mbkgr/abmod/item/ModItems.java
@@ -0,0 +1,36 @@
+package net.mbkgr.abmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.mbkgr.abmod.ABMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+
+
+
+public class ModItems {
+        // THIS IS HOW YOU REGISTER ITEM TO MINECRAFT.
+        // ItemGroup.MISC THAT DEFINES WHICH INVENTORY TAB TO REGISTER.
+        // AFTER REGISTER, YOU NEED TO ADD SOME CODE TO en_us.json FILE. THERE IS AN EXAMPLE OVER THERE.
+        // THEN YOU NEED TO CREATE A JSON FILE FOR ITEM LIKE example_item.json
+        // FOR LAST, ADD ITEM TEXTURE FILE TO /textures/item AT .PNG FORMAT
+        public static final Item exampleItem = registerItem("example_item",
+                new Item(new FabricItemSettings().group(ItemGroup.MISC )));
+
+
+        private static Item registerItem(String name, Item item) {
+            return Registry.register(Registry.ITEM, new Identifier(ABMod.MOD_ID, name), item);
+        }
+
+        public static void registerModItems() {
+
+
+            ABMod.LOGGER.debug("Registering Mod Items for " + ABMod.MOD_ID);
+
+        }
+
+
+
+}
