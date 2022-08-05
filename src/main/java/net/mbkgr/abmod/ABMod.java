c776181490a552248da7a2bbf2affa38ea4a12a3
Muhammed Bakgor <bakgormuhammed@gmail.com>
2022-08-05 19:07:34 +0300

    Initial commit

---
+++ src/main/java/net/mbkgr/abmod/ABMod.java
@@ -0,0 +1,18 @@
+package net.mbkgr.abmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ABMod implements ModInitializer {
+
+	public static final String MOD_ID = "abmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/mbkgr/abmod/ABModClient.java
@@ -0,0 +1,10 @@
+package net.mbkgr.abmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ABModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
